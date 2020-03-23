f2889ee6ab42fcf540336a73384ec1e64c4c96c1
Alexander Do <alexdo@gmail.com>
2020-03-22 17:23:56 -0700

    Add optional priority for unsafe routes

--- tun_linux.go
+++ tun_linux.go
@@ -251,6 +251,10 @@ func (c Tun) Activate() error {
 			Scope:     unix.RT_SCOPE_LINK,
 		}
 
+		if r.priority != nil {
+			nr.Priority = *r.priority
+		}
+
 		err = netlink.RouteAdd(&nr)
 		if err != nil {
 			return fmt.Errorf("failed to set mtu %v on route %v; %v", r.mtu, r.route, err)
